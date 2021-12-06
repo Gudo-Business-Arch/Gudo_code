94e1fcd393d38d8c0d605896411a9caa135d798d
frozenLake <frozenLake123@gmail.com>
2021-12-06 12:03:10 -0600

    Update main.js
    Now has a number of comments that may or may not be helpful. IDK.

--- GudoV5/main.js
+++ GudoV5/main.js
@@ -12,12 +12,9 @@ const SpeechRecognition =
 const SpeechGrammarList =
   window.SpeechGrammarList || window.webkitSpeechGrammarList; // if none exists -> undefined
 const recognition = new SpeechRecognition();
-	recognition.onstart = function () {
-		console.log('Gudo is listening');
-	}
 
 recognition.onstart = function () {
-    console.log('Gudo is listening');
+	console.log('Gudo is listening');	// If this doesn't pop up, there might be problems.
 }
 
 recognition.onresult = function(event) {
@@ -31,10 +28,10 @@ recognition.onresult = function(event) {
 	// ^^^ THIS FIXES THE BUG: THE OTHER PLACE WHERE IT WOULD DEFINE QUERY RELYS ON THE USER CHANGING THE INPUT FOR THE TEXTBOX VIA TYPING on line 49
     console.log(transcript);
 	search = true;
-    doc = nlp(query)
+    doc = nlp(query) // Copies Query to Naatural Language Processing.
     console.log("PRINTING NOUNS")
     console.log(doc.nouns().json())
-    nounlist = doc.nouns().out('array')
+    nounlist = doc.nouns().out('array')	// Returns an array of each noun in the query. [TODO: Search Photos with the query itself if no nouns are found!]
     nounlist.forEach(element => console.log(element));
     nounlist.forEach(query => SearchPhotos(query, pagenum));
 };
@@ -43,20 +40,20 @@ btn.addEventListener('click', () => {
     recognition.start();
 });
 
-const auth = "563492ad6f917000010000018ac681b5e4fb48af88579beaad86c24e";
+const auth = "563492ad6f917000010000018ac681b5e4fb48af88579beaad86c24e";	// Pexels API Key
 const next = document.querySelector(".next");
 const input = document.querySelector("input");
 const search_button = document.querySelector(".search_button");
 //pexels API MAx Pictures fetched per hour is 200 pictures. for 1 month 20,000 result requests
 
-let pagenum = 1;
-let search = false;
+let pagenum = 1;	// Initializes pagenum to 1.
+let search = false;	// I'm not sure what this does?
 //lets you search inside the api
-let query = "";
+let query = "";	// Initializes Query to Blank Value, before being replaced with transcript or search box.
 
 input.addEventListener("input", (e) => {
   //prevent closing page
-  e.preventDefault();
+  e.preventDefault();	// Not sure if this is used anymore?
   query = e.target.value;
 });
 
@@ -82,9 +79,9 @@ async function CuratedPhotos(pagenum) {
       <p>Photo : ${photo.photographer}</p>
       <button onclick="moveImage(this)">Move to Library</button>
 	  <button onclick="removeImage(this)">Remove</button>
-      `;
+      `; // HTML here has variables that get swapped out, those being the ${stuff}. Also has buttons on end so that when an image pops up, the buttons will be added with them.
 
-    document.querySelector(".gallery").appendChild(pic);
+    document.querySelector(".gallery").appendChild(pic); // Shoves the previous HTML with new variables into the Gallery HTML emlement.
   });
 }
 
@@ -109,36 +106,36 @@ async function SearchPhotos(query, pagenum) {
       <p>Photo : ${photo.photographer}</p>
       <button onclick="moveImage(this)">Move to Library</button>
 	  <button onclick="removeImage(this)">Remove</button>
-      `;
+      `; // HTML here has variables that get swapped out, those being the ${stuff}. Also has buttons on end so that when an image pops up, the buttons will be added with them.
 
-    document.querySelector(".gallery").appendChild(pic);
+    document.querySelector(".gallery").appendChild(pic); // Shoves the previous HTML with new variables into the Gallery HTML emlement.
   });
-  if (result.photos.length < 1) {
+  if (result.photos.length < 1) { // Fallback incase Pexels API fails to provide image for search, such as if the search has no results, or if we ran out our limit of pexels api calls.
 	  loadImgUnsplashed()
   }
 }
 
-search_button.addEventListener("click", () => {
+search_button.addEventListener("click", () => { // Preforms Search when clicking search button.
   if (input.value === "") return;
   search = true;
-  doc = nlp(query)
+  doc = nlp(query)	// Copies Query to Naatural Language Processing.
   console.log("PRINTING NOUNS")
   console.log(doc.nouns().json())
-  nounlist = doc.nouns().out('array')
+  nounlist = doc.nouns().out('array')	// Returns an array of each noun in the query. [TODO: Search Photos with the query itself if no nouns are found!]
   nounlist.forEach(element => console.log(element));
   nounlist.forEach(query => SearchPhotos(query, pagenum));
   ;
   pagenum++;
 });
 
-document.querySelector('input').addEventListener('keypress', function (e) {
+document.querySelector('input').addEventListener('keypress', function (e) {	// Does search when hitting Enter.
   if (e.key === 'Enter') {
     if (input.value === "") return;
     search = true;
-    doc = nlp(query)
+    doc = nlp(query)	// Copies Query to Naatural Language Processing.
     console.log("PRINTING NOUNS")
     console.log(doc.nouns().json())
-    nounlist = doc.nouns().out('array')
+    nounlist = doc.nouns().out('array')	// Returns an array of each noun in the query. [TODO: Search Photos with the query itself if no nouns are found!]
     nounlist.forEach(element => console.log(element));
     nounlist.forEach(query => SearchPhotos(query, pagenum));
     pagenum++;
@@ -149,35 +146,36 @@ document.querySelector('input').addEventListener('keypress', function (e) {
 //  document.querySelector(".gallery").innerHTML = "";
 //}
 
-next.addEventListener("click", () => {
+next.addEventListener("click", () => { // Preforms Next Page Search when clicking next button.
   //if false and we dont have any input and you hit the next button then the page will load the next images, if true there is a value in the search input and it will return the desired input
-  if (!search) {
+  if (!search) { // If we don't have a search, have it pull up the next page of curated photos... which appears to be broken.
     pagenum++;
     CuratedPhotos(pagenum);
   } else {
     if (query.value === "") return;
     pagenum++;
-    doc = nlp(query)
+	// hey, shouldn't we stick this chunk into its own function, to minimize repetitive code? ...Yeah, we totally should, shouldn't we.
+    doc = nlp(query)	// Copies Query to Naatural Language Processing.
     console.log("PRINTING NOUNS")
     console.log(doc.nouns().json())
-    nounlist = doc.nouns().out('array')
+    nounlist = doc.nouns().out('array') // Returns an array of each noun in the query. [TODO: Search Photos with the query itself if no nouns are found!]
     nounlist.forEach(element => console.log(element));
     nounlist.forEach(query => SearchPhotos(query, pagenum));
   }
 });
-CuratedPhotos(pagenum);
+CuratedPhotos(pagenum); // ...Does this ever get called? I don't know.
 
-function loadImgUnsplashed() {
+function loadImgUnsplashed() {	// Image loading for unsplashed, in case Pexel fails to work.
 
   const urlUnsplashed =
     "https://api.unsplash.com/search/photos/?query=" +
     input.value +
-    "&per_page=1&client_id=YeGuXHx1Qvm9MhEQ8A8YTSp89UX-N5nus9awY5NdrfA";
+    "&per_page=1&client_id=YeGuXHx1Qvm9MhEQ8A8YTSp89UX-N5nus9awY5NdrfA";	// Behold, the API Key.
 
   fetch(urlUnsplashed)
     .then((response) => {
       if (response.ok) return response.json();
-      else alert(response.status);
+      else alert(response.status);	// Uh oh, something went wrong... somewhere.
     })
     .then((data) => {
       const pic = document.createElement("div");
@@ -186,18 +184,18 @@ function loadImgUnsplashed() {
 		<p>Photo : Unsplashed</p>
 		<button onclick="moveImage(this)">Move to Library</button>
 		<button onclick="removeImage(this)">Remove</button>
-		`;
+		`; // The HTML appears again, this time specifying that the Photo is from Unsplashed, as that doesn't have the Author information Pexel does.
         document.querySelector(".gallery").appendChild(pic);
 	  }
       })
     };
 
-function removeImage(elem) {
+function removeImage(elem) { // Called when pressing the remove button. Removes image, Details on image, and buttons.
   elem.parentNode.remove();
 }
 
-function moveImage(elem) {
+function moveImage(elem) { // Called when pressing move to library button. 
   var source = elem.parentNode;
-  library.appendChild(source);
+  library.appendChild(source); // I think this is the line of code responsible for removing the Move to Library button once in the Library?
   elem.remove()
 }
